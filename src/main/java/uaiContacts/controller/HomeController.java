af4b58e90188b6adbee1a6196eb837d09c02edc3
uaihebert <contato@uaihebert.com>
2014-06-16 11:00:23 -0300

    editing import

--- src/main/java/uaiContacts/controller/HomeController.java
+++ src/main/java/uaiContacts/controller/HomeController.java
@@ -1,5 +1,7 @@
 package uaiContacts.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
